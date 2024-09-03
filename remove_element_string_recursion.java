6be39cdaea26e2cabe745aed7b373bf448d92256
Aayush Anand <aayushanand27@gmail.com>
2024-09-03 19:46:01 +0530

    Update remove_element_string_recursion.java

--- remove_element_string_recursion.java
+++ remove_element_string_recursion.java
@@ -1,3 +1,20 @@
 public class remove_element_string_recursion {
-    ff
+    public static String remove(String str,int i){
+        if(i ==str.length()){
+            return "";
+        }
+        String SmallAns = remove(str, i+1);
+        char CurrChar = str.charAt(i);
+
+        if(str.charAt(i)!='a'){
+            return CurrChar+SmallAns;
+        }
+        else{
+            return SmallAns;
+        }
+    }
+    public static void main(String[] args) {
+        String str = "Aayush";
+        System.out.println(remove(str, 0));
+    }
 }
