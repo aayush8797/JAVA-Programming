d01b44ee19198efbdef037555896fe5d6e06a658
Aayush Anand <aayushanand27@gmail.com>
2024-09-12 17:28:52 +0530

    Update Convert_into_string.java

--- Convert_into_string.java
+++ Convert_into_string.java
@@ -3,13 +3,10 @@ public class Convert_into_string {
         "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"
     };
 
-    // Recursive function to convert number to words
     public static String numberToWords(int number) {
-        // Base case: if the number is a single digit
         if (number < 10) {
             return DIGITS[number];
         }
-        // Recursive case: process the last digit and call the function for the rest of the number
         return numberToWords(number / 10) + " " + DIGITS[number % 10];
     }
 
